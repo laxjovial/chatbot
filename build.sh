bb9f129e9fed1b5d133b1b7fd00f2d4e8df1d866
laxjovial <laxjovial@gmail.com>
2025-06-19 14:34:03 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,4 @@
-#!/bin/bash
-
-# Optional: remove NLTK data if not needed anymore
-rm -rf nltk_data
-
-# Install dependencies
+#!/usr/bin/env bash
 pip install -r requirements.txt
-
-# Download spaCy models
 python -m spacy download en_core_web_trf
 python -m spacy download xx_ent_wiki_sm
