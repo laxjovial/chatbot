e80a07d4ec29f673f7dbf3841028a39f02cd7d70
laxjovial <laxjovial@gmail.com>
2025-06-19 18:19:15 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,3 @@
-#!/usr/bin/env bash
-pip install -r requirements.txt
+#!/bin/bash
 python -m spacy download en_core_web_trf
 python -m spacy download xx_ent_wiki_sm
-python -c "import matplotlib.pyplot as plt"
