468f8da2376bb07e8d02d3841e7a7ae5f4abc851
laxjovial <laxjovial@gmail.com>
2025-06-19 19:20:47 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,5 +3,4 @@ pip install --upgrade pip
 # Step 1: Install all required packages
 pip install -r requirements.txt
 
-# Step 2: Download spaCy models
-python -m spacy download xx_ent_wiki_sm
+# Step 2: Download spaCy 
